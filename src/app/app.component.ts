8e2fba31977ab2f27f7aa4f48835ba6518fc983c
Ryan Radder <ryan.radder@protonmail.com>
2022-07-29 19:48:01 +0200

    -added routing and view.compnent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mandelbrot-web-workers';
+  events: string[] = [];
+  opened = true;
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ViewComponent} from "./components/view/view.component";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {FormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
